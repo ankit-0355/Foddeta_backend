c757abd100b6522bbc89dbdcecbd4d6a83255528
Ankit Prajapati <ankitprajapati@mac.home>
2025-11-29 16:51:31 -0500

    Database setup

--- app/app.py
+++ app/app.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from app.database.db_core import Database
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get('/')
+origins = [
+        "http://localhost:4200",  # Example: your frontend running on localhost
+    ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,  # Allow cookies, authorization headers, etc.
+    allow_methods=["*"],     # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],     # Allow all headers in the request
+    )
+
+@app.get("/")
 def root():
     return ("Welcome to Foodeta backend")
 
+@app.get("/database")
+def read_database():
+    db = Database()
+    response = db.execute_query()
+    return response
---
+++ app/database/db_core.py
@@ -0,0 +1,23 @@
+import supabase
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+class Database:
+    def __init__(self):
+        self.client = supabase.supabase_url
+    
+    def execute_query(self):
+        response = (
+            supabase.table("Foodeta")
+            .select("*")
+            .execute()
+        )
+        data_received = response.data
+        return data_received
